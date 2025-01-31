a0eba50787de6f431ddf5603016a5406da029840
kefir228 <oleg05karp@gmail.com>
2025-01-31 15:19:59 +0200

    debug2

--- src/app/game/multiplayer/page.tsx
+++ src/app/game/multiplayer/page.tsx
@@ -5,7 +5,7 @@ export default function MultiplayerGame() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-700">
       <h2 className="text-2xl font-bold mb-4">Гра 1v1</h2>
-      <Board />
+      <Board isBotGame={false}/>
     </div>
   );
 }
